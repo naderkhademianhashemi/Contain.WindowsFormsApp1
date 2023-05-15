ff4d2dd662c7ed858ced83a6781956267b2ab287
PC 2 <PC 2@DESKTOP-8ELQAGK>
2023-05-15 10:13:40 +0430

    Add project files.

---
+++ Contain.WindowsFormsApp1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contain.WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            bool containsThree = numbers.Contains(3);
+            MessageBox.Show(containsThree.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var f = new char[] { 'k', 'f' };
+            var r = f.Contains("bf"[1]);
+            var x = new char[] { 'k', 'f' }.Contains((null ?? "bf").ToLower()[1]);
+            MessageBox.Show(r.ToString());
+        }
+    }
+}
